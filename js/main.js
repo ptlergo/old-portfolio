67c30edfa976efca16c37d4eb4f01138abcc29aa
ptlergo <ptlergo@gmail.com>
2015-06-12 02:21:27 -0400

    functionality fixed
    divs are dynamic using jQuery

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	//about button show more/less
+	$(".about_buttonD").click(function(){
+		$(".about").animate({height: '40vh'});
+		$(".about section").slideDown();
+	});
+	$(".about_buttonU").click(function(){
+		$(".about").animate({height: '10vh'});
+		$(".about section").slideUp();
+	});
+	//work button show more/less
+	$(".works_buttonD").click(function(){
+		$(".works").animate({height: '40vh'});
+		$(".works section").slideDown();
+	});
+	$(".works_buttonU").click(function(){
+		$(".works").animate({height: '10vh'});
+		$(".works section").slideUp();
+	});
+	//resume button show more/less
+	$(".resume_buttonD").click(function(){
+		$(".resume").animate({height: '40vh'});
+		$(".resume section").slideDown();
+	});
+	$(".resume_buttonU").click(function(){
+		$(".resume").animate({height: '10vh'});
+		$(".resume section").slideUp();
+	});
+});
